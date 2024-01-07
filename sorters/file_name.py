7a79a8f11da9489cd75f7a4549a04624c3eb4d17
JakubZeslawski <147949808+JakubZeslawski@users.noreply.github.com>
2024-01-07 20:40:47 +0100

    Create file_name.py
    sortowanie po nazwie

---
+++ sorters/file_name.py
@@ -0,0 +1,8 @@
+import os
+show_name = "Po nazwie"
+
+def sort(files: list[str]):
+    print(list)
+    new_dir_structure = {}
+    new_dir_structure[""] = sorted(files, key=str.lower)
+    return new_dir_structure
