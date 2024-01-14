1aac20b4877e09a0b37411f0ca011d2616de1c2b
JakubZeslawski <147949808+JakubZeslawski@users.noreply.github.com>
2024-01-14 20:42:45 +0100

    Update file_name.py

--- sorters/file_name.py
+++ sorters/file_name.py
@@ -1,7 +1,6 @@
 import os
-
 show_name = "Po nazwie"
-
+#Program zwraca folder alfabetycznie, bez zwracania uwagi na wielkość liter
 def sort(files: list[str]):
     new_dir_structure = {}
     new_dir_structure[""] = sorted(files, key=str.lower)
